16748073331198afe7e7c812bcc19917fc33e82a
dugf <c.eduardo.g.f.santos@gmail.com>
2022-08-13 14:16:42 -0300

    desenvolvido layout da tela favorite e acrescentado widget safearea na tela bottom navigation

--- app_mepoupe/lib/bloc/ui/screens/favorite_screen.dart
+++ app_mepoupe/lib/bloc/ui/screens/favorite_screen.dart
@@ -5,6 +5,174 @@ class FavoriteScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.green);
+    final queryData = MediaQuery.of(context);
+
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Theme.of(context).primaryColor.withOpacity(0.2),
+            Colors.white,
+          ],
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(
+            top: 30.0, left: 30.0, right: 30.0, bottom: 0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Flexible(
+              flex: 1,
+              child: Align(
+                alignment: Alignment.topLeft,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Image.asset(
+                      'assets/icons/star_stroke_icon.png',
+                      fit: BoxFit.fill,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                    Text(
+                      'Meus favoritos',
+                      style: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                          fontFamily: 'Poppins',
+                          fontSize: queryData.size.longestSide * 0.034,
+                          fontWeight: FontWeight.w600),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 7,
+              child: ListView(
+                children: [
+                  Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24.0),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(18.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text(
+                                '88330-301',
+                                style: TextStyle(fontSize: 16),
+                              ),
+                              InkWell(
+                                child: Image.asset(
+                                  'assets/icons/delete_icon.png',
+                                  fit: BoxFit.fill,
+                                  width: 20,
+                                  height: 20,
+                                ),
+                                onTap: () {},
+                              ),
+                            ],
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.symmetric(vertical: 8.0),
+                            child: FittedBox(
+                              child: Text(
+                                  'Rua indaial - até 583 - Itajaí SC -CEP\n88303-301'),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24.0),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(18.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text(
+                                '88330-301',
+                                style: TextStyle(fontSize: 16),
+                              ),
+                              InkWell(
+                                child: Image.asset(
+                                  'assets/icons/delete_icon.png',
+                                  fit: BoxFit.fill,
+                                  width: 20,
+                                  height: 20,
+                                ),
+                                onTap: () {},
+                              ),
+                            ],
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.symmetric(vertical: 8.0),
+                            child: FittedBox(
+                              child: Text(
+                                  'Rua indaial - até 583 - Itajaí SC -CEP\n88303-301'),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24.0),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(18.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text(
+                                '88330-301',
+                                style: TextStyle(fontSize: 16),
+                              ),
+                              InkWell(
+                                child: Image.asset(
+                                  'assets/icons/delete_icon.png',
+                                  fit: BoxFit.fill,
+                                  width: 20,
+                                  height: 20,
+                                ),
+                                onTap: () {},
+                              ),
+                            ],
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.symmetric(vertical: 8.0),
+                            child: FittedBox(
+                              child: Text(
+                                  'Rua indaial - até 583 - Itajaí SC -CEP\n88303-301'),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
--- app_mepoupe/lib/bloc/ui/widgets/bottom_navigation_widget.dart
+++ app_mepoupe/lib/bloc/ui/widgets/bottom_navigation_widget.dart
@@ -23,38 +23,40 @@ class _BottomNavigationWidgetState extends State<BottomNavigationWidget> {
   Widget build(BuildContext context) {
     final screenIndexProvider = Provider.of<ScreenIndexProvider>(context);
     int currentScreenIndex = screenIndexProvider.fetchCurrentScreenIndex;
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: screens[currentScreenIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-        selectedItemColor: Theme.of(context).primaryColor,
-        selectedLabelStyle: const TextStyle(fontSize: 12),
-        currentIndex: currentScreenIndex,
-        onTap: (value) => screenIndexProvider.updateScreenIndex(value),
-        items: const [
-          BottomNavigationBarItem(
-            icon: ImageIcon(
-              AssetImage('assets/icons/home_icon.png'),
-              size: 20,
-            ),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: ImageIcon(
-              AssetImage('assets/icons/flag_home_icon.png'),
-              size: 20,
-            ),
-            label: 'Procurar',
-          ),
-          BottomNavigationBarItem(
+    return SafeArea(
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: screens[currentScreenIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          selectedItemColor: Theme.of(context).primaryColor,
+          selectedLabelStyle: const TextStyle(fontSize: 12),
+          currentIndex: currentScreenIndex,
+          onTap: (value) => screenIndexProvider.updateScreenIndex(value),
+          items: const [
+            BottomNavigationBarItem(
               icon: ImageIcon(
-                AssetImage('assets/icons/star_stroke_icon.png'),
+                AssetImage('assets/icons/home_icon.png'),
                 size: 20,
               ),
-              label: 'Favoritos'),
-        ],
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: ImageIcon(
+                AssetImage('assets/icons/flag_home_icon.png'),
+                size: 20,
+              ),
+              label: 'Procurar',
+            ),
+            BottomNavigationBarItem(
+                icon: ImageIcon(
+                  AssetImage('assets/icons/star_stroke_icon.png'),
+                  size: 20,
+                ),
+                label: 'Favoritos'),
+          ],
+        ),
       ),
     );
   }
