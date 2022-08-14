9b7f12af4ccfa98746919c7cf5a5a4bbac542733
dugf <c.eduardo.g.f.santos@gmail.com>
2022-08-14 18:30:22 -0300

    portrait screens

--- app_mepoupe/lib/main.dart
+++ app_mepoupe/lib/main.dart
@@ -2,15 +2,26 @@ import 'package:app_mepoupe/bloc/address_manager.dart';
 import 'package:app_mepoupe/bloc/screen_index_provider.dart';
 import 'package:app_mepoupe/views/widgets/bottom_navigation_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Than we setup preferred orientations,
+  // and only after it finished we run our app
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((value) => runApp(const MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
